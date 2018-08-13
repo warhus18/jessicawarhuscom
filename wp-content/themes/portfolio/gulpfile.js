1adb10bd61abb2ea702b0234f402690c89552c2b
Jessica <jessica@o3world.com>
2018-08-12 21:26:58 -0400

    Remove style guide

--- wp-content/themes/portfolio/gulpfile.js
+++ wp-content/themes/portfolio/gulpfile.js
@@ -7,7 +7,6 @@ const autoprefixer = require('gulp-autoprefixer'),
       gulp = require('gulp'),
       imagemin = require('gulp-imagemin'),
       notify = require('gulp-notify'),
-      nunjucksrender = require('gulp-nunjucks-render'),
       plumber = require('gulp-plumber'),
       rename = require('gulp-rename'),
       sass = require('gulp-ruby-sass'),
@@ -125,11 +124,6 @@ gulp.task('svg-sprite-build', gulp.series(function() {
                 },
                 prefix: '.u-icon-%s',
                 sprite: '../../../../images/sprite-ui.svg', //generated sprite name
-                example: {
-                    template: 'style-guide/templates/icon-template/symbol.tpl.html',
-                    dest: 'iconography.njk'
-                },
-                dest: 'style-guide/templates/pages/elements/'
             }
         }
     };
@@ -148,70 +142,13 @@ gulp.task('svg-sprite-build', gulp.series(function() {
     return svgStream;
 }));
 
-gulp.task('svg-sprite-copy', gulp.series(['svg-sprite-build'], function() {
-    return gulp.src('images/sprite-ui.svg')
-        .pipe(gulp.dest('style-guide/templates/assets'))
-        .pipe(notify({
-            message: '"sprite-ui.svg" Copied to Design System',
-            onLast: true
-        }));
-}));
-
-gulp.task('svg-sprite', gulp.series(['svg-sprite-build', 'svg-sprite-copy']));
-
-gulp.task('styles-styleguide', gulp.series(function() {
-    return sass([
-        'styles/style-guide.scss'
-    ], {
-        noCache: true,
-        style: 'compact',
-        sourcemap: true
-    })
-    .pipe(plumber())
-    .pipe(autoprefixer({
-        browsers: ['last 2 versions', 'IE 11', 'Safari 8']
-    }))
-    .pipe(cleancss({
-        keepSpecialComments: false,
-        processImport: false
-    }))
-    .pipe(autoprefixer({
-        browsers: ['last 2 versions', 'IE 11', 'Safari 8']
-    }))
-    .pipe(rename({
-        suffix: '.min'
-    }))
-    .pipe(sourcemaps.write('maps', {
-        includeContent: false,
-        sourceRoot: '/styles'
-    }))
-    .pipe(gulp.dest('styles'))
-    .pipe(notify({
-        message: '"Styles - Style Guide" Task Completed',
-        onLast: true
-    }));
-}));
-
-gulp.task('nunjucks', gulp.series(function() {
-    return gulp.src('style-guide/templates/pages/**/*.+(html|njk)')
-    .pipe(data(function() {
-        return require('./style-guide/data.json');
-    }))
-    .pipe(nunjucksrender({
-        path: ['style-guide/templates']
-    }))
-    .pipe(gulp.dest('style-guide'))
-    .pipe(notify({
-        message: '"Nunjucks" Task Completed'
-    }));
-}));
+gulp.task('svg-sprite', gulp.series(['svg-sprite-build']));
 
 gulp.task('watch', gulp.series(function() {
-    gulp.watch(['styles/**/*.scss', 'styles/*.scss'], gulp.parallel(['styles-site', 'styles-styleguide', 'styles-sass-lint']));
+    gulp.watch(['styles/**/*.scss', 'styles/*.scss'], gulp.parallel(['styles-site', 'styles-sass-lint']));
     gulp.watch('images/svg/*.svg', gulp.parallel(['svg-sprite']));
     gulp.watch(['scripts/source/vendor/**/**/*.js', 'scripts/source/vendor/**/*.js', 'scripts/source/vendor/*.js'], gulp.parallel(['scripts-plugins']));
     gulp.watch('scripts/source/functionality/*.js', gulp.parallel(['scripts', 'scripts-js-lint']));
-    gulp.watch('style-guide/templates/**/*.+(html|njk)', gulp.parallel(['nunjucks']));    
 }));
 
 gulp.task('default', gulp.series(['styles-site', 'styles-sass-lint', 'scripts', 'scripts-js-lint', 'scripts-plugins']));
